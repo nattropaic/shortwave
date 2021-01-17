4971787704b174a14d5ba7f3c6c2f2a17efa0b0e
nattropaic <3751933+nattropaic@users.noreply.github.com>
2021-01-16 17:13:43 -0800

    First go at a private podcast feed maker

---
+++ shortwave.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+from urllib.parse import urljoin
+
+from feedgen.feed import FeedGenerator
+import hjson
+
+
+def generate(args):
+    with args.feedfile as f:
+        feeddata = hjson.load(f)
+
+    feed = FeedGenerator()
+    feed.load_extension("podcast")
+
+    base_url = feeddata["url"]
+
+    feed.title(feeddata["title"])
+    feed.language(feeddata.get("language", "en"))
+    feed.author(name=feeddata["author"])
+    feed.link(href=base_url, rel="alternate")
+    feed.logo(urljoin(base_url, feeddata["logo"]))
+    # subtitle
+
+    # These feeds are private.
+    feed.podcast.itunes_block(True)
+
+    for episodedata in feeddata["episodes"]:
+        episode_url = urljoin(base_url, episodedata["path"])
+
+        episode = feed.add_entry()
+        episode.id(episode_url)
+        episode.title(episodedata["title"])
+        episode.description(episodedata["description"])
+        episode.link(href=episodedata["link"])
+        episode.enclosure(episode_url)
+
+    feed.rss_str(pretty=True)
+    feed.atom_file("atom.xml")
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description="Generate a podcast feed for some audio files.")
+    parser.add_argument(
+        "feedfile",
+        required=False,
+        type=argparse.FileType("r"),
+        default="./feed.hjson",
+        help="Config file to generate a feed from (default: feed.hjson)",
+    )
+
+    args = parser.parse_args(argv)
+
+    generate(args)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
